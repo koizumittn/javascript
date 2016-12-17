dc4ecaecdc14d786152304bc1c0c42301c8e580f
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-17 16:06:22 +0900

    ボタンクリックのイベントから関数を実行する。

--- test-js/app/assets/javascripts/top.js
+++ test-js/app/assets/javascripts/top.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+function printHelloWithButton() {
+  var btn = document.querySelector("input");
+  // ボタンをDOMオブジェクトとして取得し、変数btnに代入する
+
+  function printHello() {
+    console.log("Hello World!")
+  }
+
+  btn.addEventListener("click", printHello);
+  // イベントリスナをbtn(DOMオブジェクト)に対して作成
+  // クリックイベントを捉えて、printHello()メソッドをコールする
+}
+
+window.addEventListener("load", printHelloWithButton);
+// loadイベントはページが全てロードされたタイミングで発生する
+// これによりページを全て読み込んだ後にprintHelloWithButtonを呼び出している
