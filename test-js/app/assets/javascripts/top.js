11878776aeeb92f0dddafe2b9637caa0946f2a3c
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-17 17:17:15 +0900

    ボタンクリックによりHTMLのテキストを変更する。

--- test-js/app/assets/javascripts/top.js
+++ test-js/app/assets/javascripts/top.js
@@ -2,13 +2,13 @@ window.addEventListener("load", function()  {
     var btn = document.querySelector("input");
   // ボタンをDOMオブジェクトとして取得し、変数btnに代入する
 
-  function printHello() {
-    console.log("Hello World!")
-  }
-
-  btn.addEventListener("click", printHello);
   // イベントリスナをbtn(DOMオブジェクト)に対して作成
-  // クリックイベントを捉えて、printHello()メソッドをコールする
+  // クリックイベントを捉えて、記述した関数を実行する
+  btn.addEventListener("click", function() {
+    var p = document.querySelector("p")
+    p.innerText = "クリックされた！";
+    // innerTextによりp要素のテキストを取得、変更できる
+  });
 });
 // loadイベントはページが全てロードされたタイミングで発生する
 // これによりページを全て読み込んだ後にprintHelloWithButtonを呼び出している
