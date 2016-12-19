2000a09fdc07dc27f202e201d4cf2d2bde43bcdf
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-19 19:50:40 +0900

    show/hideメソッドを用いて、タブ切り替え時に表示される内容を変更する。 hide() = display: none;と等価。 show() = display: blockやdisplay: inlineなど状況に応じて作成。

--- test-js/app/assets/javascripts/top.js
+++ test-js/app/assets/javascripts/top.js
@@ -2,5 +2,7 @@ $(window).on("load", function() {
   $("li").on("click", function() {
     $("li.selected").removeClass("selected");
     $(this).addClass("selected");
+    $(".contents div").hide();
+    $(".contents ." + this.id).show();
   });
 });
